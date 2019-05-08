48fa58d77219ccad52db0929d250f402e3d1f2d2
htbkoo <h1017760@connect.hku.hk>
2019-05-08 23:05:50 +0800

    added diagnostic messages

--- public/service-worker.ts
+++ public/service-worker.ts
@@ -1,4 +1,20 @@
 console.log("@service-worker.js");
+
+const URL_PATTERN = /https:\/\/lib\.hku\.hk\/hours/;
+
+const handlerCb = ({url, event, params}) => {
+    return fetch(event.request)
+        .then((response) => {
+            console.log(`got response`);
+            return response.text();
+        })
+        .then((responseBody) => {
+            //return new Response(`${responseBody} <!-- Look Ma. Added Content. -->`);
+            console.log(`parsed and get body: ${responseBody}`);
+            return new Response(responseBody);
+        });
+};
+
 // @ts-ignore
 if (workbox) {
     console.log(`Workbox is loaded`);
@@ -6,7 +22,10 @@ if (workbox) {
     // @ts-ignore
     workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-}
-else {
+    console.log(`registering for url pattern: ${URL_PATTERN.toString()}`);
+
+    // @ts-ignore
+    workbox.routing.registerRoute(URL_PATTERN, handlerCb);
+} else {
     console.log(`Workbox didn't load`);
 }
